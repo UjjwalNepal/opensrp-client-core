56bd855cbcd7d37719c9c27453bba990f8f48122
Samuel Githengi <samuel.githengi@gmail.com>
2018-12-10 19:34:54 +0300

    Fix sync

--- opensrp-app/src/main/java/org/smartregister/sync/intent/LocationIntentService.java
+++ opensrp-app/src/main/java/org/smartregister/sync/intent/LocationIntentService.java
@@ -12,9 +12,11 @@ import org.smartregister.domain.Location;
 import org.smartregister.domain.Response;
 import org.smartregister.repository.AllSharedPreferences;
 import org.smartregister.repository.LocationRepository;
+import org.smartregister.repository.StructureRepository;
 import org.smartregister.service.HTTPAgent;
 import org.smartregister.sync.helper.SyncIntentServiceHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.smartregister.AllConstants.OPERATIONAL_AREAS;
@@ -22,8 +24,10 @@ import static org.smartregister.AllConstants.OPERATIONAL_AREAS;
 public class LocationIntentService extends IntentService {
     public static final String LOCATION_STRUCTURE_URL = "/rest/location/sync";
     public static final String STRUCTURES_LAST_SYNC_DATE = "STRUCTURES_LAST_SYNC_DATE";
+    public static final String LOCATION_LAST_SYNC_DATE = "LOCATION_LAST_SYNC_DATE";
     private static final String TAG = LocationIntentService.class.getCanonicalName();
     private LocationRepository locationRepository;
+    private StructureRepository structureRepository;
     private AllSharedPreferences allSharedPreferences = CoreLibrary.getInstance().context().allSharedPreferences();
 
     public LocationIntentService() {
@@ -32,24 +36,62 @@ public class LocationIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        syncLocationsStructures(true);
+        syncLocations();
         syncLocationsStructures(false);
     }
 
 
     protected void syncLocationsStructures(boolean is_jurisdiction) {
-        long serverVersion = Long.parseLong(allSharedPreferences.getPreference(STRUCTURES_LAST_SYNC_DATE));
+        long serverVersion = 0;
         try {
-            JSONArray tasksResponse = fetchLocationsOrStructures(is_jurisdiction, serverVersion);
-            List<Location> locations = SyncIntentServiceHelper.parseTasksFromServer(tasksResponse, Location.class);
+            serverVersion = Long.parseLong(allSharedPreferences.getPreference(is_jurisdiction ? LOCATION_LAST_SYNC_DATE : STRUCTURES_LAST_SYNC_DATE));
+        } catch (NumberFormatException e) {
+        }
+        try {
+            List<String> parentIds = locationRepository.getAllLocationIds();
+            List<Location> locations = new ArrayList<>();
+            for (String parentId : parentIds) {
+                JSONArray tasksResponse = fetchLocationsOrStructures(is_jurisdiction, serverVersion, parentId);
+                locations.addAll(SyncIntentServiceHelper.parseTasksFromServer(tasksResponse, Location.class));
+            }
             for (Location location : locations) {
                 try {
-                    locationRepository.addOrUpdate(location);
+                    if (is_jurisdiction)
+                        locationRepository.addOrUpdate(location);
+                    else
+                        structureRepository.addOrUpdate(location);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            allSharedPreferences.savePreference(String.valueOf(geMaxServerVersion(locations, serverVersion)), STRUCTURES_LAST_SYNC_DATE);
+            allSharedPreferences.savePreference(String.valueOf(geMaxServerVersion(locations, serverVersion)), is_jurisdiction ? LOCATION_LAST_SYNC_DATE : STRUCTURES_LAST_SYNC_DATE);
+
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
+
+    protected void syncLocations() {
+        boolean is_jurisdiction = true;
+        long serverVersion = 0;
+        try {
+            serverVersion = Long.parseLong(allSharedPreferences.getPreference(is_jurisdiction ? LOCATION_LAST_SYNC_DATE : STRUCTURES_LAST_SYNC_DATE));
+        } catch (NumberFormatException e) {
+        }
+        try {
+            JSONArray tasksResponse = fetchLocationsOrStructures(is_jurisdiction, serverVersion, null);
+            List<Location> locations = SyncIntentServiceHelper.parseTasksFromServer(tasksResponse, Location.class);
+            for (Location location : locations) {
+                try {
+                    if (is_jurisdiction)
+                        locationRepository.addOrUpdate(location);
+                    else
+                        structureRepository.addOrUpdate(location);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            allSharedPreferences.savePreference(String.valueOf(geMaxServerVersion(locations, serverVersion)), is_jurisdiction ? LOCATION_LAST_SYNC_DATE : STRUCTURES_LAST_SYNC_DATE);
 
         } catch (Exception e) {
             Log.e(TAG, e.getMessage(), e);
@@ -57,7 +99,7 @@ public class LocationIntentService extends IntentService {
     }
 
 
-    private String makeURL(boolean is_jurisdiction, long serverVersion) {
+    private String makeURL(boolean is_jurisdiction, long serverVersion, String parentId) {
         String baseUrl = CoreLibrary.getInstance().context().
                 configuration().dristhiBaseURL();
         String endString = "/";
@@ -66,14 +108,13 @@ public class LocationIntentService extends IntentService {
         }
         if (is_jurisdiction) {
             String preferenceLocationNames = allSharedPreferences.getPreference(OPERATIONAL_AREAS);
-            return baseUrl + LOCATION_STRUCTURE_URL + "?is_jurisdiction=" + is_jurisdiction + "&location_names=" + preferenceLocationNames;
+            return baseUrl + LOCATION_STRUCTURE_URL + "?is_jurisdiction=" + is_jurisdiction + "&location_names=" + preferenceLocationNames + "&serverVersion=" + serverVersion;
         }
-        String parent_ids = android.text.TextUtils.join(",", locationRepository.getAllLocationIds());
-        return baseUrl + LOCATION_STRUCTURE_URL + "?parent_id=" + parent_ids + "&is_jurisdiction=" + is_jurisdiction + "&serverVersion=" + serverVersion;
+        return baseUrl + LOCATION_STRUCTURE_URL + "?parent_id=" + parentId + "&is_jurisdiction=" + is_jurisdiction + "&serverVersion=" + serverVersion;
 
     }
 
-    private JSONArray fetchLocationsOrStructures(boolean is_jurisdiction, Long serverVersion) throws Exception {
+    private JSONArray fetchLocationsOrStructures(boolean is_jurisdiction, Long serverVersion, String parentId) throws Exception {
 
         HTTPAgent httpAgent = CoreLibrary.getInstance().context().getHttpAgent();
         if (httpAgent == null) {
@@ -81,7 +122,7 @@ public class LocationIntentService extends IntentService {
             throw new IllegalArgumentException(LOCATION_STRUCTURE_URL + " http agent is null");
         }
 
-        Response resp = httpAgent.fetch(makeURL(is_jurisdiction, serverVersion));
+        Response resp = httpAgent.fetch(makeURL(is_jurisdiction, serverVersion, parentId));
         if (resp.isFailure()) {
             sendBroadcast(SyncIntentServiceHelper.completeSync(FetchStatus.nothingFetched));
             throw new NoHttpResponseException(LOCATION_STRUCTURE_URL + " not returned data");
@@ -105,6 +146,7 @@ public class LocationIntentService extends IntentService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         locationRepository = CoreLibrary.getInstance().context().getLocationRepository();
+        structureRepository = CoreLibrary.getInstance().context().getStructureRepository();
         return super.onStartCommand(intent, flags, startId);
     }
 
--- opensrp-app/src/main/java/org/smartregister/sync/intent/SyncTaskIntentService.java
+++ opensrp-app/src/main/java/org/smartregister/sync/intent/SyncTaskIntentService.java
@@ -2,6 +2,7 @@ package org.smartregister.sync.intent;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.apache.http.NoHttpResponseException;
@@ -11,6 +12,7 @@ import org.smartregister.domain.FetchStatus;
 import org.smartregister.domain.Response;
 import org.smartregister.domain.Task;
 import org.smartregister.repository.AllSharedPreferences;
+import org.smartregister.repository.LocationRepository;
 import org.smartregister.repository.TaskRepository;
 import org.smartregister.service.HTTPAgent;
 import org.smartregister.sync.helper.SyncIntentServiceHelper;
@@ -26,6 +28,7 @@ public class SyncTaskIntentService extends IntentService {
     private static final String TAG = SyncTaskIntentService.class.getCanonicalName();
     private TaskRepository taskRepository;
     private AllSharedPreferences allSharedPreferences = CoreLibrary.getInstance().context().allSharedPreferences();
+    private LocationRepository locationRepository;
 
     public SyncTaskIntentService() {
         super("SyncTaskIntentService");
@@ -38,8 +41,12 @@ public class SyncTaskIntentService extends IntentService {
 
     protected void syncTasks() {
         String campaigns = allSharedPreferences.getPreference(CAMPAIGNS);
-        String groups = allSharedPreferences.getPreference(OPERATIONAL_AREAS);
-        long serverVersion = Long.parseLong(allSharedPreferences.getPreference(TASK_LAST_SYNC_DATE));
+        String groups = TextUtils.join(",", locationRepository.getAllLocationIds());
+        long serverVersion = 0;
+        try {
+            serverVersion = Long.parseLong(allSharedPreferences.getPreference(TASK_LAST_SYNC_DATE));
+        } catch (NumberFormatException e) {
+        }
         try {
             JSONArray tasksResponse = fetchTasks(campaigns, groups, serverVersion);
             List<Task> tasks = SyncIntentServiceHelper.parseTasksFromServer(tasksResponse, Task.class);
@@ -51,12 +58,8 @@ public class SyncTaskIntentService extends IntentService {
                     e.printStackTrace();
                 }
             }
-            allSharedPreferences.savePreference(String.valueOf(geMaxServerVersion(tasks, serverVersion)),TASK_LAST_SYNC_DATE);
+            allSharedPreferences.savePreference(String.valueOf(geMaxServerVersion(tasks, serverVersion)), TASK_LAST_SYNC_DATE);
 
-            Task task = taskRepository.getTaskByIdentifier("tsk11231jh23");
-
-
-            Log.i("Structures ","STruc "+task.getBusinessStatus());
 
         } catch (Exception e) {
             Log.e(TAG, e.getMessage(), e);
@@ -91,6 +94,7 @@ public class SyncTaskIntentService extends IntentService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         taskRepository = CoreLibrary.getInstance().context().getTaskRepository();
+        locationRepository = CoreLibrary.getInstance().context().getLocationRepository();
         return super.onStartCommand(intent, flags, startId);
     }
 
